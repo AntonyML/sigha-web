866718c2d347157b574f40cf4371440033db5c59
Antony Monge Lopez <antony.mongelopez@ucr.ac.cr>
2026-07-05 16:31:30 -0600

    refactor: Remove unused Electron main and preload scripts; update Tailwind CSS forms plugin import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import forms from '@tailwindcss/forms'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class'],
@@ -78,5 +80,5 @@ export default {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [forms],
 }
